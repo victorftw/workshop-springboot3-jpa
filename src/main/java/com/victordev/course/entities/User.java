59819f6cf7a62165f12b9595fb7d755da1adec9e
Victor Hugo <victor.hiraiftw@outlook.com>
2023-10-09 15:47:47 -0300

    feat: h2 database, test profile, JPA

--- src/main/java/com/victordev/course/entities/User.java
+++ src/main/java/com/victordev/course/entities/User.java
@@ -1,5 +1,10 @@
 package com.victordev.course.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -11,10 +16,14 @@ import java.util.Objects;
  * </p>
  */
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
