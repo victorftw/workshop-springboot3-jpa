ea4b7f126e4bbfbece32fb7509c110c651a4b4b4
Victor Hugo <victor.hiraiftw@outlook.com>
2023-10-10 12:06:07 -0300

    feat: exception handling update

--- src/main/java/com/victordev/course/services/UserService.java
+++ src/main/java/com/victordev/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.victordev.course.entities.User;
 import com.victordev.course.repositories.UserRepository;
 import com.victordev.course.services.exceptions.DatabaseException;
 import com.victordev.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -68,9 +69,13 @@ public class UserService {
    */
 
   public User update(Long id, User obj) {
-    User entity = repository.getReferenceById(id);
-    updateData(entity, obj);
-    return repository.save(entity);
+    try {
+      User entity = repository.getReferenceById(id);
+      updateData(entity, obj);
+      return repository.save(entity);
+    } catch (EntityNotFoundException e) {
+      throw new ResourceNotFoundException(id);
+    }
   }
 
   private void updateData(User entity, User obj) {
