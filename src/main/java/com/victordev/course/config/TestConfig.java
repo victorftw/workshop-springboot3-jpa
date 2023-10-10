d6bda59f3ec65e19d7a8a5d891679d3561001772
Victor Hugo <victor.hiraiftw@outlook.com>
2023-10-10 09:58:48 -0300

    feat: payment, one-to-one association

--- src/main/java/com/victordev/course/config/TestConfig.java
+++ src/main/java/com/victordev/course/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.victordev.course.config;
 import com.victordev.course.entities.Category;
 import com.victordev.course.entities.Order;
 import com.victordev.course.entities.OrderItem;
+import com.victordev.course.entities.Payment;
 import com.victordev.course.entities.Product;
 import com.victordev.course.entities.User;
 import com.victordev.course.entities.enums.OrderStatus;
@@ -91,5 +92,10 @@ public class TestConfig implements CommandLineRunner {
     OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
     orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+    Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+    o1.setPayment(pay1);
+
+    orderRepository.save(o1);
   }
 }
